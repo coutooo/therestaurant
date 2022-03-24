cdc0378046170673f85d1680e54e56ec99d7aefa
Rafael Dias <rafaeldsd93@gmail.com>
2022-03-24 15:45:38 +0000

    interveningEntities start

---
+++ src/InterveningEntities/Chief.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package InterveningEntities;
+
+/**
+ * Chief thread:
+ * Implements the life-cycle of a chief and stores his internal variables
+ * and his state during his lifecycle.
+ * 
+ * @author Rafael Dias
+ * @author Manuel Couto
+ */
+public class Chief extends Thread {
+    
+}
---
+++ src/InterveningEntities/Student.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package InterveningEntities;
+
+/**
+ * Student thread:
+ * Implements the life-cycle of a student and stores his internal variables
+ * and his state during his lifecycle.
+ * 
+ * @author Rafael Dias
+ * @author Manuel Couto
+ */
+public class Student extends Thread {
+    
+}
