bbe19212cc50e2a47d312afe7a0897aee16eceb1
Rafael Dias <rafaeldsd93@gmail.com>
2022-04-20 23:43:18 +0100

    Update student entity

--- src/InterveningEntities/Student.java
+++ src/InterveningEntities/Student.java
@@ -4,6 +4,9 @@
  */
 package InterveningEntities;
 
+import sharedRegions.*;
+
+
 /**
  * Student thread:
  * Implements the life-cycle of a student and stores his internal variables
@@ -14,32 +17,121 @@ package InterveningEntities;
  */
 public class Student extends Thread {
     
-     private int id;
+    /**
+     *   Chief identification
+     */
+    private int student_id;
     
-    private StudentState state;
+    /**
+     * 	Student state
+    */
+    private StudentState currentState;
     
+    /**
+     *   Reference to Bar
+     */
+    private final Bar b;
     
+    /**
+     *   Reference to Table
+     */
+    private final Table t;
     
-    public void run(){
-        walkABit();
+    /**
+     *   Instantiation of a Student thread.
+     *
+     *     @param name thread name
+     *     @param student_id student id
+     *     @param b reference to the Bar
+     *     @param k reference to the Kitchen
+     */
+    public Student(String name, int student_id, Bar b, Table t){
+        super(name);
+        this.student_id = student_id;
+        this.currentState = StudentState.GOING_TO_THE_RESTAURANT;
+        this.b = b;
+        this.t = t;
     }
-    
-    public void walkABit(){
-        
-    }
-    
-    public int geStudentID() {
-		return id;
+    /**
+     *   Life cycle of the Chief.
+     */
+    @Override
+	public void run (){
+            walkABit();
+            b.enter();
+            t.readMenu();
+
+            if(studentId == t.getFirstToArrive()){
+                    t.prepareOrder();
+                    while(!t.everybodyHasChosen())
+                            t.addUpOnesChoices();
+                    b.callWaiter();
+                    t.describeOrder();
+                    t.joinTalk();
+            }
+            else{t.informCompanion();}
+ 
+            int numCoursesEaten = 0;
+            while(!t.haveAllCoursesBeenEaten()){
+                    t.startEating();
+                    t.endEating();
+                    numCoursesEaten++;
+
+                    while(!t.hasEverybodyFinishedEating());
+                    System.out.println("I FINISHED MY MEAL");
+                    if(studentId == t.getLastToEat() && numCoursesEaten != ExecuteConst.M) {b.signalWaiter();}
+            }
+
+            if(t.shouldHaveArrivedEarlier()) {
+                    b.signalWaiter();
+                    t.honourBill();
+            }
+            b.exit();
 	}
     
-    public StudentState getStudentState(){
-        return state;
+    private void walkABit() {
+        try
+        { sleep ((long) (1 + 100 * Math.random ()));
+        }
+        catch (InterruptedException e) {}
     }
     
-    public void setState(StudentState s) {
-		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-		state = s;
-	}
     
+    /**
+     *   Set Student id.
+     *
+     *     @param id Student id
+     */
+    public void setStudentID(StudentState state){
+        currentState = state;
+    }
     
+    /**
+     *   Get Student id.
+     *
+     *     @return Student id
+     */
+    public int getStudentID() {
+        return student_id;
+    }
+    
+    /**
+     *   Set Student state.
+     *
+     *     @param state Student state
+     */
+    public void setStudentState (StudentState state)
+    {
+        currentState = state;
+    }
+
+    /**
+     *   Get Student state.
+     *
+     *     @return Student state.
+     */
+    public StudentState getStudentState ()
+    {
+        return currentState;
+    }
 }
