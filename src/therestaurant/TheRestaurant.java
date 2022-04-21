fa7549957353c1d9e47c602a947da0016297df16
Rafael Dias <rafaeldsd93@gmail.com>
2022-04-21 15:59:07 +0100

    iniciar main

--- src/therestaurant/TheRestaurant.java
+++ src/therestaurant/TheRestaurant.java
@@ -6,10 +6,13 @@ package therestaurant;
 import java.util.Date;
 import sharedRegions.*;
 import InterveningEntities.*;
+import genclass.GenericIO;
+import genclass.TextFile;
 
 /**
- *
- * @author Couto and Rafinha
+ *  Simulation of the Problem of The restaurant
+ * 
+ * @author Couto and Rafael
  */
 public class TheRestaurant {
 
@@ -20,7 +23,7 @@ public class TheRestaurant {
     
     /**
      * Number of Courses  (three courses per participant in the
-dinner: a starter, a main course and a dessert.)
+     * dinner: a starter, a main course and a dessert.)
      */
     public static final int Ncourses = 3;
     
@@ -29,17 +32,35 @@ dinner: a starter, a main course and a dessert.)
     */
     public static final String filename = "log_" + new Date().toString().replace(' ', '_') + ".txt";
     
+    /**
+   *    Main method.
+   *
+   *    @param args runtime arguments
+   */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        // Shared memory regions
-        Bar bar = new Bar();
-        Kitchen kitchen = new Kitchen();
-        Table table = new Table();
+        Chef chef;						//Reference to the Chef Thread
+        Waiter waiter;                                          //Reference to the Waiter Thread
+        Student[] student = new Student[Nstudents];    		//array of references to the Students Threads
+        Bar b;							//Reference to the Bar
+        Kitchen k;						//Reference to the Kitchen
+        Table t;						//Reference to the Table
+        GeneralRepos repos;                                     //Reference to the General Repository
         
-        // Entities
-        Student[] students = new Student[Nstudents];
-        
+        System.out.println("\nThe restaurant");
+        /* problem initialization */
+        /*
+        repos = new GeneralRepos("logger");
+        departureAirport = new DepartureAirport(repos);
+        plane = new Plane(repos);
+        destinationAirport = new DestinationAirport(repos);
+
+        pilot = new Pilot("Pilot_1", 0, departureAirport, plane, destinationAirport, repos);
+        hostess = new Hostess("Hostess_1", 0, departureAirport, plane, repos);
+        for (int i = 0; i < ExecConst.N; i++)
+            passenger[i] = new Passenger("Passenger_"+(i+1), i, departureAirport, destinationAirport);
+        */
+        /* start of the simulation */
         
         
     }
