5d369d91f21b5df41db137eb292149acb8ed09f7
coutooo <manuelcouto10@ua.pt>
2022-05-29 05:09:27 +0100

    scripts fixed

--- ASS2_T2_G10/src/buildAndGenerateGlobal.sh
+++ ASS2_T2_G10/src/buildAndGenerateGlobal.sh
@@ -5,19 +5,19 @@ echo "  General Repository of Information"
 rm -rf dirGeneralRepos
 mkdir -p dirGeneralRepos dirGeneralRepos/serverSide dirGeneralRepos/serverSide/main dirGeneralRepos/serverSide/entities dirGeneralRepos/serverSide/sharedRegions \
          dirGeneralRepos/clientSide dirGeneralRepos/clientSide/entities dirGeneralRepos/commInfra
-cp serverSide/main/ExecConst.class serverSide/main/ServerGeneralReposMain.class dirGeneralRepos/serverSide/main
+cp serverSide/main/ExecConst.class serverSide/main/ServerRestaurantGeneralRepos.class dirGeneralRepos/serverSide/main
 cp serverSide/entities/GeneralReposClientProxy.class dirGeneralRepos/serverSide/entities
 cp serverSide/sharedRegions/GeneralReposInterface.class serverSide/sharedRegions/GeneralRepos.class dirGeneralRepos/serverSide/sharedRegions
-cp clientSide/entities/ChefStates.class clientSide/entities/StudentStates.class clientSide/entities/WaiterStates.class dirGeneralRepos/clientSide/entities
+cp clientSide/entities/ChefState.class clientSide/entities/StudentState.class clientSide/entities/WaiterState.class dirGeneralRepos/clientSide/entities
 cp commInfra/Message.class commInfra/MessageType.class commInfra/MessageException.class commInfra/ServerCom.class dirGeneralRepos/commInfra
 echo "  Bar"
 rm -rf dirBar
 mkdir -p dirBar dirBar/serverSide dirBar/serverSide/main dirBar/serverSide/entities dirBar/serverSide/sharedRegions \
          dirBar/clientSide dirBar/clientSide/entities dirBar/clientSide/stubs dirBar/commInfra
-cp serverSide/main/ExecConst.class serverSide/main/ServerBarMain.class dirBar/serverSide/main
+cp serverSide/main/ExecConst.class serverSide/main/ServerRestaurantBar.class dirBar/serverSide/main
 cp serverSide/entities/BarClientProxy.class dirBar/serverSide/entities
 cp serverSide/sharedRegions/GeneralReposInterface.class serverSide/sharedRegions/BarInterface.class serverSide/sharedRegions/Bar.class dirBar/serverSide/sharedRegions
-cp clientSide/entities/ChefStates.class clientSide/entities/StudentStates.class clientSide/entities/WaiterStates.class clientSide/entities/ChefCloning.class \
+cp clientSide/entities/ChefState.class clientSide/entities/StudentState.class clientSide/entities/WaiterState.class clientSide/entities/ChefCloning.class \
    clientSide/entities/StudentCloning.class clientSide/entities/WaiterCloning.class dirBar/clientSide/entities
 cp clientSide/stubs/GeneralReposStub.class dirBar/clientSide/stubs
 cp commInfra/*.class dirBar/commInfra
@@ -25,10 +25,10 @@ echo "  Table"
 rm -rf dirTable
 mkdir -p dirTable dirTable/serverSide dirTable/serverSide/main dirTable/serverSide/entities dirTable/serverSide/sharedRegions \
          dirTable/clientSide dirTable/clientSide/entities dirTable/clientSide/stubs dirTable/commInfra
-cp serverSide/main/ExecConst.class serverSide/main/ServerTableMain.class dirTable/serverSide/main
+cp serverSide/main/ExecConst.class serverSide/main/ServerRestaurantTable.class dirTable/serverSide/main
 cp serverSide/entities/TableClientProxy.class dirTable/serverSide/entities
 cp serverSide/sharedRegions/GeneralReposInterface.class serverSide/sharedRegions/TableInterface.class serverSide/sharedRegions/Table.class dirTable/serverSide/sharedRegions
-cp clientSide/entities/ChefStates.class clientSide/entities/StudentStates.class clientSide/entities/WaiterStates.class clientSide/entities/ChefCloning.class \
+cp clientSide/entities/ChefState.class clientSide/entities/StudentState.class clientSide/entities/WaiterState.class clientSide/entities/ChefCloning.class \
    clientSide/entities/StudentCloning.class clientSide/entities/WaiterCloning.class dirTable/clientSide/entities
 cp clientSide/stubs/GeneralReposStub.class dirTable/clientSide/stubs
 cp commInfra/*.class dirTable/commInfra
@@ -36,10 +36,10 @@ echo "  Kitchen"
 rm -rf dirKitchen
 mkdir -p dirKitchen dirKitchen/serverSide dirKitchen/serverSide/main dirKitchen/serverSide/entities dirKitchen/serverSide/sharedRegions \
          dirKitchen/clientSide dirKitchen/clientSide/entities dirKitchen/clientSide/stubs dirKitchen/commInfra
-cp serverSide/main/ExecConst.class serverSide/main/ServerKitchenMain.class dirKitchen/serverSide/main
+cp serverSide/main/ExecConst.class serverSide/main/ServerRestaurantKitchen.class dirKitchen/serverSide/main
 cp serverSide/entities/KitchenClientProxy.class dirKitchen/serverSide/entities
 cp serverSide/sharedRegions/GeneralReposInterface.class serverSide/sharedRegions/KitchenInterface.class serverSide/sharedRegions/Kitchen.class dirKitchen/serverSide/sharedRegions
-cp clientSide/entities/ChefStates.class clientSide/entities/StudentStates.class clientSide/entities/WaiterStates.class clientSide/entities/ChefCloning.class \
+cp clientSide/entities/ChefState.class clientSide/entities/StudentState.class clientSide/entities/WaiterState.class clientSide/entities/ChefCloning.class \
    clientSide/entities/StudentCloning.class clientSide/entities/WaiterCloning.class dirKitchen/clientSide/entities
 cp clientSide/stubs/GeneralReposStub.class dirKitchen/clientSide/stubs
 cp commInfra/*.class dirKitchen/commInfra
@@ -48,8 +48,8 @@ rm -rf dirChef
 mkdir -p dirChef dirChef/serverSide dirChef/serverSide/main dirChef/clientSide dirChef/clientSide/main dirChef/clientSide/entities \
          dirChef/clientSide/stubs dirChef/commInfra
 cp serverSide/main/ExecConst.class dirChef/serverSide/main
-cp clientSide/main/ClientChefMain.class dirChef/clientSide/main
-cp clientSide/entities/Chef.class clientSide/entities/ChefStates.class dirChef/clientSide/entities
+cp clientSide/main/ChefMain.class dirChef/clientSide/main
+cp clientSide/entities/Chef.class clientSide/entities/ChefState.class dirChef/clientSide/entities
 cp clientSide/stubs/GeneralReposStub.class clientSide/stubs/BarStub.class clientSide/stubs/KitchenStub.class clientSide/stubs/TableStub.class dirChef/clientSide/stubs
 cp commInfra/Message.class commInfra/MessageType.class commInfra/MessageException.class commInfra/ClientCom.class dirChef/commInfra
 echo "  Waiter"
@@ -57,8 +57,8 @@ rm -rf dirWaiter
 mkdir -p dirWaiter dirWaiter/serverSide dirWaiter/serverSide/main dirWaiter/clientSide dirWaiter/clientSide/main dirWaiter/clientSide/entities \
          dirWaiter/clientSide/stubs dirWaiter/commInfra
 cp serverSide/main/ExecConst.class dirWaiter/serverSide/main
-cp clientSide/main/ClientWaiterMain.class dirWaiter/clientSide/main
-cp clientSide/entities/Waiter.class clientSide/entities/WaiterStates.class dirWaiter/clientSide/entities
+cp clientSide/main/WaiterMain.class dirWaiter/clientSide/main
+cp clientSide/entities/Waiter.class clientSide/entities/WaiterState.class dirWaiter/clientSide/entities
 cp clientSide/stubs/GeneralReposStub.class clientSide/stubs/BarStub.class clientSide/stubs/KitchenStub.class clientSide/stubs/TableStub.class dirChef/clientSide/stubs
 cp commInfra/Message.class commInfra/MessageType.class commInfra/MessageException.class commInfra/ClientCom.class dirWaiter/commInfra
 echo "  Students"
@@ -66,8 +66,8 @@ rm -rf dirStudents
 mkdir -p dirStudents dirStudents/serverSide dirStudents/serverSide/main dirStudents/clientSide dirStudents/clientSide/main dirStudents/clientSide/entities \
          dirStudents/clientSide/stubs dirStudents/commInfra
 cp serverSide/main/ExecConst.class dirStudents/serverSide/main
-cp clientSide/main/ClientStudentMain.class dirStudents/clientSide/main
-cp clientSide/entities/Student.class clientSide/entities/StudentStates.class dirStudents/clientSide/entities
+cp clientSide/main/StudentMain.class dirStudents/clientSide/main
+cp clientSide/entities/Student.class clientSide/entities/StudentState.class dirStudents/clientSide/entities
 cp clientSide/stubs/GeneralReposStub.class clientSide/stubs/BarStub.class clientSide/stubs/KitchenStub.class clientSide/stubs/TableStub.class dirChef/clientSide/stubs
 cp commInfra/Message.class commInfra/MessageType.class commInfra/MessageException.class commInfra/ClientCom.class dirStudents/commInfra
 echo "Compressing execution environments."
@@ -91,4 +91,4 @@ rm -f  dirWaiter.zip
 zip -rq dirWaiter.zip dirWaiter
 echo "  Students"
 rm -f  Students.zip
-zip -rq Students.zip dirStudents
+zip -rq dirStudents.zip dirStudents
--- ASS2_T2_G10/src/testar.sh
+++ ASS2_T2_G10/src/testar.sh
@@ -3,13 +3,13 @@ javac -cp /home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclas
 
 
 
- gnome-terminal --title="ServerRestaurantGeneralRepos" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantGeneralRepos 22169 
- gnome-terminal --title="ServerRestaurantBar" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantBar 22160 127.0.0.1 22169 127.0.0.1 22162 
- gnome-terminal --title="ServerRestaurantKitchen" -- java  -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantKitchen 22161 127.0.0.1 22169 
- gnome-terminal --title="ServerRestaurantTable" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantTable 22162 127.0.0.1 22169 
+ gnome-terminal --title="ServerRestaurantGeneralRepos" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantGeneralRepos 22110 
+ gnome-terminal --title="ServerRestaurantBar" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantBar 22111 127.0.0.1 22110 127.0.0.1 22112
+ gnome-terminal --title="ServerRestaurantKitchen" -- java  -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantKitchen 22113 127.0.0.1 22110 
+ gnome-terminal --title="ServerRestaurantTable" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar serverSide.main.ServerRestaurantTable 22112 127.0.0.1  22110
   
  sleep 1 
- gnome-terminal --title="WaiterMain" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar clientSide.main.WaiterMain 127.0.0.1 22160 127.0.0.1 22162 127.0.0.1 22161 127.0.0.1 22169 logger 
- gnome-terminal --title="ChefMain" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar clientSide.main.ChefMain 127.0.0.1 22160 127.0.0.1 22161
+ gnome-terminal --title="WaiterMain" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar clientSide.main.WaiterMain 127.0.0.1 22113 127.0.0.1 22111 127.0.0.1 22112 127.0.0.1 22110
+ gnome-terminal --title="ChefMain" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar clientSide.main.ChefMain  127.0.0.1 22113 127.0.0.1 22111 127.0.0.1 22110
  sleep 1 
- gnome-terminal --title="StudentMain" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar clientSide.main.StudentMain 127.0.0.1 22160 127.0.0.1 22169
+ gnome-terminal --title="StudentMain" -- java -cp .:/home/couto/Desktop/Universidade/therestaurant/ASS2_T2_G10/src/genclass.jar clientSide.main.StudentMain 127.0.0.1 22111 127.0.0.1 22112 127.0.0.1 22110
